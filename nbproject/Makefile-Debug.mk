c9c4256d1ed6924738d0a82ee4171d0dd2781e14
Joe Andaverde <joe@andaverde.net>
2012-11-19 00:57:01 -0600

    Updated config to point to included lib

--- nbproject/Makefile-Debug.mk
+++ nbproject/Makefile-Debug.mk
@@ -52,7 +52,7 @@ FFLAGS=
 ASFLAGS=
 
 # Link Libraries and Options
-LDLIBSOPTIONS=-L../arduino_corelib/dist/Debug/Arduino-MacOSX -larduino_corelib -lm
+LDLIBSOPTIONS=-Llib/arduino_corelib -lm -larduino_corelib
 
 # Build Targets
 .build-conf: ${BUILD_SUBPROJECTS}
